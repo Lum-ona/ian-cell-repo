7bc740bfa0358399ff18307d08b37044c602c3e2
Lum-ona <lumonamulengwa@gmail.com>
2022-09-23 00:08:01 +0300

    done

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import keras
 import imghdr
 import numpy as np
 import tensorboard
@@ -26,28 +27,20 @@ def eliminate_dodgy_img():
                 print('Issue with Image {}'.format(image_path))
 # eliminate_dodgy_img()
 
-def load_data():
+def load_n_preprocess_data():
     data = tf.keras.utils.image_dataset_from_directory('cell_images')
-    data_iterator = data.as_numpy_iterator()
-    batch = data_iterator.next()
-    return data
-# load_data()
+    scaled_data = data.map(lambda x,y: (x/255,y))
 
-def preprocess_data():
-    data = load_data()
-    data = data.map(lambda x,y: (x/255,y))
-    scaled_iterator = data.as_numpy_iterator().next()
+    train_size = int(len(scaled_data)*.7)+1
+    val_size = int(len(scaled_data)* .2)
+    test_size = int(len(scaled_data)* .1)
 
-    train_size = int(len(data)*.7)+1
-    val_size = int(len(data)* .2)
-    test_size = int(len(data)* .1)
-
-    train = data.take(train_size)
-    val = data.skip(train_size).take(val_size)
-    test = data.skip(train_size+val_size).take(test_size)
+    train = scaled_data.take(train_size)
+    val = scaled_data.skip(train_size).take(val_size)
+    test = scaled_data.skip(train_size+val_size).take(test_size)
+    print(len(train))
 
     return train, val
-# preprocess_data()
 
 def net():
     model = Sequential()
@@ -66,21 +59,42 @@ def net():
     model.add(Dense(256, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
 
-    # model.summary()
+    model.summary()
     return model
-# net()
+
 
 def train():
     model = net()
     model.compile('adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy'])
-    train, val = preprocess_data()
+    
     logdir = 'logs'
+    train, val = load_n_preprocess_data()
     tensorboard_callback = tf.keras.callbacks.TensorBoard(logdir)
-    model.fit(train, epochs=2, validation_data=val, callbacks=[tensorboard_callback])
+    
+    model.fit(train, epochs=20, validation_data=val, callbacks=[tensorboard_callback])
 
-    model.save(os.path.join('models', 'cellparasitized.h5'))
+    return model
+# train().save(os.path.join('models', 'cellmodel-v1.h5'))
 
-# train()
+
+def test ():
+    img = cv2.imread(os.path.join(DATA_DIR,'Uninfected', "C237ThinF_IMG_20151127_105345_cell_73.png"))
+    resize = tf.image.resize(img, (256,256))  
+    model = keras.models.load_model('models/cellmodel-v1.h5')
+    yhat = model.predict(np.expand_dims(resize/255,0))
+
+    #0 -> Parasitized
+    #1 -> Uninfected
+
+    if yhat < 0.5:
+        print(f'Parasitized cell!')
+    else:
+        print(f'Uninfected cell!')
+
+    print(yhat)
+   
+test()
+    
 
 
 
